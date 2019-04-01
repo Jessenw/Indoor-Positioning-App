08282d3dcd1a01ed6744450603dacc86788822cf
jessenw <Jessenw@outlook.com>
2019-04-01 21:14:12 +1300

    Implemented AccessPoint data gathering, removed ActionBar

---
+++ app/src/main/java/com/example/indoorpositionassignment/AccessPoint.java
@@ -0,0 +1,49 @@
+package com.example.indoorpositionassignment;
+
+import java.util.Comparator;
+
+public class AccessPoint implements Comparator<AccessPoint> {
+
+    private String SSID;
+    private String BSSID;       // MAC Address
+    private int    level;       // signal strength
+    private double distance;    // approximate distance to AP
+
+    public AccessPoint(String SSID, String BSSID, int level, double distance) {
+        this.SSID = SSID;
+        this.BSSID = BSSID;
+        this.level = level;
+        this.distance = distance;
+    }
+
+    public String getSSID() {
+        return this.SSID;
+    }
+
+    public String getBSSID() {
+        return BSSID;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+
+    public int compare(AccessPoint o1, AccessPoint o2) {
+        double distance1 = o1.getDistance();
+        double distance2 = o2.getDistance();
+
+        if (distance1 > distance2) return -1;
+        else if (distance1 < distance2) return 1;
+        else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return getSSID() + " : " + getBSSID() + " : " + String.format("%.2f", getDistance());
+    }
+}
--- app/src/main/java/com/example/indoorpositionassignment/MainActivity.java
+++ app/src/main/java/com/example/indoorpositionassignment/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.indoorpositionassignment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    int MY_PERMISSION_FINE_LOCATION = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request location permissions
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+
+        // Check if location permissions are granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+            System.out.println("ACCESS_FINE_LOCATION: PERMISSION_GRANTED");
+        else
+            System.out.println("ACCESS_FINE_LOCATION: PERMISSION_DENIED");
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
