83b836de8827c2500f1c6010fd6bfe04cca07819
jessenw <Jessenw@outlook.com>
2019-04-02 10:36:44 +1300

    Added map to canvas

--- app/src/main/java/com/example/indoorpositionassignment/Map.java
+++ app/src/main/java/com/example/indoorpositionassignment/Map.java
@@ -1,8 +1,13 @@
 package com.example.indoorpositionassignment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -34,10 +39,8 @@ public class Map extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Canvas canvas = new Canvas();
-        canvas.drawCircle(10, 10, 2, new Paint());
-
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        // return inflater.inflate(R.layout.fragment_map, container, false);
+        return new MyView(getActivity());
     }
 
     public void onButtonPressed(Uri uri) {
@@ -65,5 +68,42 @@ public class Map extends Fragment {
 
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
+    }
+
+    public class MyView extends View {
+
+         Paint paint;
+
+         public MyView(Context context) {
+             super(context);
+             paint = new Paint();
+         }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+             super.onDraw(canvas);
+
+             // Draw map
+             Bitmap map = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.cotton_level_2);
+
+             double scale = 2.1; // How much to scale map image by
+             int right = (int) (437 * scale);
+             int bottom = (int) (710 * scale);
+             // Rotate map
+             Matrix matrix = new Matrix();
+             matrix.postRotate(-90.0f);
+             Bitmap rotatedMap = Bitmap.createBitmap(map, 0, 0, map.getWidth(), map.getHeight(), matrix, true);
+             // Add map to canvas
+             canvas.drawBitmap(rotatedMap, null, new Rect(100, 0, right, bottom), null);
+
+
+//             int x = getWidth();
+//             int y = getHeight();
+//             int radius;
+//             radius = 100;
+//             // Use Color.parseColor to define HTML colors
+//             paint.setColor(Color.parseColor("#CD5C5C"));
+//             canvas.drawCircle(x / 2, y / 2, radius, paint);
+         }
     }
 }
